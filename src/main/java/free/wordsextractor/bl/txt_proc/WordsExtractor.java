74133d9ee83f71d11fb57f44f8827710d238eee8
Daniel Haimov <saratoga8@gmail.com>
2018-03-01 12:57:16 +0200

    Added comments and params check

--- src/main/java/free/wordsextractor/bl/txt_proc/WordsExtractor.java
+++ src/main/java/free/wordsextractor/bl/txt_proc/WordsExtractor.java
@@ -1,16 +1,21 @@
 package free.wordsextractor.bl.txt_proc;
 
+import free.wordsextractor.bl.WordExtractorException;
+
 import java.nio.file.Path;
 import java.util.List;
 
 /**
- * Created by saratoga on 10/02/18.
+ * Words extractor
  */
 public class WordsExtractor {
     final private List paths;
 
-    public WordsExtractor(final List<Path> paths) {
-        this.paths = paths;
+    public WordsExtractor(final List<Path> paths) throws WordExtractorException {
+        if ((paths != null) && !paths.isEmpty())
+            this.paths = paths;
+        else
+            throw new WordExtractorException("The list of files paths is NULL or EMPTY");
     }
 
 
