415cf41bc9186ffecc92dd1fea587d488c590c67
Daniel Haimov <saratoga8@gmail.com>
2018-03-06 17:28:04 +0200

    Added tests

--- src/main/java/free/wordsextractor/bl/txt_proc/Dictionary.java
+++ src/main/java/free/wordsextractor/bl/txt_proc/Dictionary.java
@@ -1,10 +1,18 @@
 package free.wordsextractor.bl.txt_proc;
 
 import com.drew.lang.annotations.NotNull;
+import free.wordsextractor.bl.WordsExtractorException;
+import free.wordsextractor.bl.file_proc.extractors.TextExtractorInterface;
 import org.apache.commons.lang.StringUtils;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.StandardOpenOption;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Hashtable;
@@ -22,13 +30,14 @@ public class Dictionary {
      * Constructor
      */
     public Dictionary() {
-        this.wordsStat = new Hashtable<String, Integer>();
+        this.wordsStat = new Hashtable<>();
     }
 
     /**
-     * Convert the dictionary to a string
+     * Convert the dictionary to a SORTED string
      * @return The string representation of the dictionary
      */
+    @NotNull
     public String toString() {
         final List<String> wordsSet = new ArrayList<>(wordsStat.keySet());
         Collections.sort(wordsSet, (String str1, String str2) -> str1.compareToIgnoreCase(str2));
@@ -63,13 +72,45 @@ public class Dictionary {
      * @param word The word for stripping
      * @return Stripped word
      */
+    @NotNull
     private String stripAllExceptChars(String word) {
-        return word.replaceAll("^\\W+|\\W+$", "").replaceAll("^\\d+|\\d+$", "");
+        String PUNCTUATIONS_CHARS_EDGES_REG = "^\\W+|\\W+$", NUM_CHARS_EDGES_REG = "^\\d+|\\d+$";
+        return word.replaceAll(PUNCTUATIONS_CHARS_EDGES_REG, "").replaceAll(NUM_CHARS_EDGES_REG, "");
     }
 
-    public void save(String path) {
+    /**
+     * Save the dictionary
+     * @param path The path of the file for saving the dictionary
+     * @throws WordsExtractorException
+     */
+    @NotNull
+    public void save(String path) throws WordsExtractorException {
+        log.debug ("Saving dictionary to the file " + path);
+
+        if (!wordsStat.isEmpty()) {
+            File file = new File(path);
+            if (file.exists()) {
+                try (BufferedWriter writer = Files.newBufferedWriter(file.toPath(), Charset.forName(TextExtractorInterface.CHAR_SET), StandardOpenOption.CREATE)) {
+                    writer.write(toString());
+                    writer.flush();
+                }
+                catch (IOException e) {
+                    throw new WordsExtractorException("Can't write dictionary to file: " + e);
+                }
+            }
+            else
+                throw new WordsExtractorException("The path " + path + " doesn't exist");
+        }
+        else
+            throw new WordsExtractorException("There are no words in the dictionary");
     }
 
+    /**
+     * Check a given word is in the dictionary
+     * @param word The checked word
+     * @return true - The word is in the dictionary
+     */
+    @NotNull
     public boolean contains(String word) {
         return wordsStat.containsKey(word);
     }
--- src/test/java/free/wordsextractor/bl/txt_proc/DictionaryTest.java
+++ src/test/java/free/wordsextractor/bl/txt_proc/DictionaryTest.java
@@ -1,5 +1,7 @@
 package free.wordsextractor.bl.txt_proc;
 
+import free.wordsextractor.bl.WordsExtractorException;
+import free.wordsextractor.bl.file_proc.Utils;
 import org.junit.Assert;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
@@ -7,8 +9,13 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
-class DictionaryTest {
+import java.io.File;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
 
+class DictionaryTest {
+    private static Utils.Shells shell;
     private Dictionary dict;
 
     @BeforeEach
@@ -21,17 +28,17 @@ class DictionaryTest {
     public void addWords() {
         dict.addWord("one");
         Assert.assertTrue("The dictionary doesn't contain the first word", dict.contains("one"));
-        Assert.assertEquals("Statistics is invalid after adding the first word", dict.toString(), "one 1");
+        Assert.assertEquals("Statistics is invalid after adding the first word", "one 1", dict.toString());
 
         dict.addWord("two");
         Assert.assertTrue("The dictionary doesn't contain the second word", dict.contains("two"));
         Assert.assertTrue("The first word has disappeared after adding the second", dict.contains("one"));
-        Assert.assertEquals("Statistics is invalid after adding the second word", dict.toString(), "one 1two 1");
+        Assert.assertEquals("Statistics is invalid after adding the second word", "one 1two 1", dict.toString());
 
         dict.addWord("one");
-        Assert.assertEquals("Statistics is invalid after adding the first existing word", dict.toString(), "one 2two 1");
+        Assert.assertEquals("Statistics is invalid after adding the first existing word", "one 2two 1", dict.toString());
         dict.addWord("two");
-        Assert.assertEquals("Statistics is invalid after adding the second existing word", dict.toString(), "one 2two 2");
+        Assert.assertEquals("Statistics is invalid after adding the second existing word", "one 2two 2", dict.toString());
     }
 
     @DisplayName("Add words with punctuations and numbers")
@@ -53,12 +60,37 @@ class DictionaryTest {
     @ValueSource(strings = {"", "    ", ":-", " 2,", "45"})
     public void addEmptyWord() {
         dict.addWord("");
-        Assert.assertEquals(dict.toString(), "");
+        Assert.assertEquals("", dict.toString());
     }
 
     @DisplayName("Adding words to a dictionary from different threads")
     @Test
     public void differentThreads() {
-        
+        List<String> words1 = Arrays.asList("one", "two", "three", "four");
+        List<String> words2 = Arrays.asList("one", "two", "three", "four");
+
+        Arrays.asList(words1, words2).parallelStream().forEach(words -> words.parallelStream().forEach(word -> dict.addWord(word)));
+        Assert.assertEquals("four 2one 2three 2two 2", dict.toString());
+    }
+
+    @DisplayName("Save dictionary in a file")
+    @Test
+    public void saveDict() {
+        Arrays.asList("one", "two", "three", "four").parallelStream().forEach(word -> dict.addWord(word));
+        try {
+            String path = File.createTempFile("dict", ".txt").getAbsolutePath();
+            dict.save(path);
+
+            String commandStr = ""; /*
+            switch (shell) {
+                case BASH: commandStr = "cat " + path;
+                case POWERSHELL: Assert.assertTrue("For PowerShlell hasn't implemented yet", false); return "";
+                default:
+                    log.error("Unknown shell " + shell.name());
+            }
+            String out = Utils.runSystemCommand(, ) */
+        } catch (IOException | WordsExtractorException e) {
+            Assert.assertTrue("Test aborted because of exception: " + e, false);
+        }
     }
 }
