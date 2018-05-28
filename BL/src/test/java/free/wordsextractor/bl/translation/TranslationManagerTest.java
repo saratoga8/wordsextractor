202bc6bff4b2eafb5a5d24ff942425697828b94a
Daniel Haimov <saratoga8@gmail.com>
2018-05-28 23:17:15 +0300

    fixed TranslationManager.removeKnownWords

--- BL/src/test/java/free/wordsextractor/bl/translation/TranslationManagerTest.java
+++ BL/src/test/java/free/wordsextractor/bl/translation/TranslationManagerTest.java
@@ -3,6 +3,7 @@ package free.wordsextractor.bl.translation;
 import free.wordsextractor.bl.extraction.file_proc.Utils;
 import free.wordsextractor.bl.extraction.txt_proc.dictionaries.Dictionary;
 import free.wordsextractor.bl.extraction.txt_proc.dictionaries.OnlyWordsDictionary;
+import free.wordsextractor.bl.extraction.txt_proc.dictionaries.TranslationsDictionary;
 import org.junit.Assert;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -21,7 +22,12 @@ class TranslationManagerTest {
             TranslationManager mngr = new TranslationManager(dict);
             Assert.assertEquals("[six, four, one, seven, two, three, five, eight]", mngr.getExtractedWordsDict().getWords().toString());
 
-            mngr.removeKnownWords(Paths.get(this.getClass().getClassLoader().getResource(TranslationManager.KNOWN_WORDS_FILE_NAME).toURI()));
+            TranslationsDictionary knownWords = new TranslationsDictionary();
+            knownWords.addTranslation("one", "number 1");
+            knownWords.addTranslation("two", "number 2");
+            knownWords.addTranslation("three", "number 3");
+            String path = knownWords.saveInTmpDir("known.dict");
+            mngr.removeKnownWords(Paths.get(path));
             Assert.assertEquals("[six, four, seven, five, eight]", mngr.getExtractedWordsDict().getWords().toString());
         } catch (IOException | URISyntaxException e) {
             System.err.println("Test has aborted because of exception: " + e);
