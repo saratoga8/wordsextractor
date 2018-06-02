a2d20a7b7fa2dcbbb2388783d808deaf69c24353
Daniel Haimov <saratoga8@gmail.com>
2018-06-02 14:59:23 +0300

    build Main in Distribution

--- Distribution/src/main/java/Main.java
+++ Distribution/src/main/java/Main.java
@@ -2,9 +2,9 @@ import free.wordsextractor.bl.WordsExtractorException;
 import free.wordsextractor.bl.extraction.file_proc.FileManager;
 import free.wordsextractor.bl.extraction.file_proc.extractors.WordsExtractor;
 import free.wordsextractor.bl.extraction.txt_proc.dictionaries.Dictionary;
+import free.wordsextractor.bl.translation.Translation;
 import free.wordsextractor.bl.translation.TranslationManager;
-import free.wordsextractor.ui.WordsExtractorGUI;
-import javafx.application.Application;
+import free.wordsextractor.bl.translation.yandex.YandexTranslation;
 
 import java.nio.file.Path;
 import java.nio.file.Paths;
@@ -12,21 +12,31 @@ import java.util.List;
 
 public class Main {
     public static void main(String [] args) {
-        String txtFilePath = args[1],
+        String txtFilePath = args[1];
+        String apiKeyPath = args[2];
+        String knownWordsPath = args[3];
 
         try {
             final FileManager fileMngr = new FileManager(txtFilePath);
             final List<Path> pathsList = fileMngr.extractTxtFiles(123);
             final WordsExtractor extractor = new WordsExtractor(pathsList);
-            Dictionary wordsStatsDict = extractor.createWordsStatsDictionary();
+            final Dictionary wordsStatsDict = extractor.createWordsStatsDictionary();
 
             final TranslationManager translationMngr = new TranslationManager(wordsStatsDict);
-            translationMngr.removeKnownWords(Paths.get(TranslationManager.KNOWN_WORDS_FILE_NAME));
-            translationMngr.getExtractedWordsDict().saveIn(extractedWordsTxtFilePath);
+            translationMngr.removeKnownWords(Paths.get(knownWordsPath));
+            final Dictionary unknownWordsDict = translationMngr.getExtractedWordsDict();
+//            wordsStatsDict = wordsStatsDict.extractWordsOfDict(unknownWordsDict);
+
+            Dictionary translations = new YandexTranslation(Paths.get(apiKeyPath), Translation.Langs.getLang("eng"), Translation.Langs.getLang("ru")).translate(unknownWordsDict.getWords());
+
+
+//            String translationsDictPath = translations.saveIn();
+
+//            Application.launch(WordsExtractorGUI.class, );
         }
         catch (WordsExtractorException e) {
             System.err.println("Running interrupted by exception " + e);
         }
-        Application.launch(WordsExtractorGUI.class, args);
+
     }
 }
