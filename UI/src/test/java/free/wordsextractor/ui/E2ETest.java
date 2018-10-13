b15ecd830f02a17db8550edc6b76721ca946dea4
Daniel Haimov <saratoga8@gmail.com>
2018-10-13 11:49:07 +0300

    updated log4j; fixed test

--- UI/src/test/java/free/wordsextractor/ui/E2ETest.java
+++ UI/src/test/java/free/wordsextractor/ui/E2ETest.java
@@ -99,8 +99,30 @@ public class E2ETest extends ApplicationTest {
             launch(WordsExtractorGUI.class, paths);
 
             TableView table = lookup("#table").queryTableView();
+            int before = listWindows().size();
             String expected = "[a 2, add 1, an 1, and 1, annotation 2, array 1, by 3, can 1, configure 2, csvsource 1, data 4, few 1, follow 1, have 3, if 1, is 1, method 3, methods 1, multiple 1, objects 1, of 1, one 1, only 1, or 1, our 3, parameterized 1, parameters 1, pass 1, provided 1, rules 1, specify 1, string 1, test 8, the 3, these 1, this 1, to 5, used 1, using 2, we 6, when 2]";
             Assert.assertEquals(expected, table.getItems().sorted().toString());
+
+            clickOn("words");
+            clickOn("add");
+            clickOn("array");
+            clickOn("can");
+            scroll(30, VerticalDirection.DOWN);
+            clickOn("using");
+            clickOn("used");
+            clickOn("#undoBtn");
+            moveTo("this");
+            scroll(30, VerticalDirection.UP);
+            clickOn("configure");
+
+            clickOn("#btnOk");
+            clickOn("#btnCancel");
+            sleep(1000);
+            Assert.assertEquals(before - 1, listWindows().size());
+
+            String content = new String(Files.readAllBytes(Paths.get(KNOWNS_FILE_NAME)));
+            expected = "add\narray\ncan\nusing\nconfigure\n";
+            Assert.assertEquals(expected, content);
         } catch (Exception e) {
             Assert.assertTrue("Test aborted because of: " + e, false);
         }
